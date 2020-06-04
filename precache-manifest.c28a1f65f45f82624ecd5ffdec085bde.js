63487a255a2f262f2b4cc62e6c408dca08b826b4
Dpessheva <dpessheva@gmail.com>
2020-06-04 19:43:20 +0200

    Updates

---
+++ precache-manifest.c28a1f65f45f82624ecd5ffdec085bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de56638974a0eaf39e063b431c86e6e",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "b01f73d05ad26218bdbd",
+    "url": "/recipe-app/static/css/main.1df5f234.chunk.css"
+  },
+  {
+    "revision": "bce376255c9f273da8ed",
+    "url": "/recipe-app/static/js/2.b3d2b4c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.b3d2b4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01f73d05ad26218bdbd",
+    "url": "/recipe-app/static/js/main.39667921.chunk.js"
+  },
+  {
+    "revision": "c0ff539ea545a33668e1",
+    "url": "/recipe-app/static/js/runtime-main.c350eb4f.js"
+  }
+]);
